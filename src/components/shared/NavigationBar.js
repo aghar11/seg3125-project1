f35c3806632857bd95fc32d690dc4eba98064d12
Akarsh Gharge <akarshgharge20@gmail.com>
2023-06-11 13:50:40 -0400

    Brought LinkContainer back

--- src/components/shared/NavigationBar.js
+++ src/components/shared/NavigationBar.js
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { NavLink } from 'react-router-dom';
+import LinkContainer from 'react-router-bootstrap/LinkContainer';
 
 function NavigationBar() {
     return (
@@ -11,31 +11,31 @@ function NavigationBar() {
         <Navbar bg="white" expand="lg">
           <Container>
             <Navbar.Brand>
-              <NavLink to="/">
+              <LinkContainer to="/">
                 <img
                   src={logo}
                   height='40'
                   className='d-inline-block align-top'
                   alt='Pitstop Perf '
                 />
-              </NavLink>
+              </LinkContainer>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link>
-                  <NavLink to='/'>Home</NavLink>
-                </Nav.Link>
+                <LinkContainer to="/">
+                  <Nav.Link>Home</Nav.Link>
+                </LinkContainer>
                 <NavDropdown title="Services" id="basic-nav-dropdown">
-                  <NavDropdown.Item>
-                    <NavLink to="/regularMaintenance">Regular Maintenance</NavLink>
-                  </NavDropdown.Item>
-                  <NavDropdown.Item>
-                    <NavLink to="/performanceModifications">Performance Modifications</NavLink>
-                  </NavDropdown.Item>
-                  <NavDropdown.Item>
-                    <NavLink to="/tuning">Tuning</NavLink>
-                  </NavDropdown.Item>
+                  <LinkContainer to="/regularMaintenance">
+                    <NavDropdown.Item>Regular Maintenance</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/performanceModifications">
+                    <NavDropdown.Item>Performance Modifications</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/tuning">
+                    <NavDropdown.Item>Tuning</NavDropdown.Item>
+                  </LinkContainer>
                 </NavDropdown>
               </Nav>
             </Navbar.Collapse>
