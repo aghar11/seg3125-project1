2f01cbd0bd38890128b99d70a448611da3cb07fe
Akarsh Gharge <akarshgharge20@gmail.com>
2023-06-11 13:10:39 -0400

    Moved BrowserRouter to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from './components/homepage/Homepage';
 import PerformanceModifications from './components/performanceModifications/PerfromanceModifications';
 import RegularMaintenance from './components/regularMaintenance/RegularMaintenance';
@@ -6,12 +6,14 @@ import Tuning from './components/tuning/Tuning';
 
 function App() {
     return (
-        <Routes>
-            <Route exact path="/" element={<Homepage/>} />
-            <Route exact path="/regularMaintenance" element={<RegularMaintenance/>} />
-            <Route exact path="/performanceModifications" element={<PerformanceModifications/>} />
-            <Route exact path="/tuning" element={<Tuning/>} />
-        </Routes>
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<Homepage/>} />
+                <Route exact path="/regularMaintenance" element={<RegularMaintenance/>} />
+                <Route exact path="/performanceModifications" element={<PerformanceModifications/>} />
+                <Route exact path="/tuning" element={<Tuning/>} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
