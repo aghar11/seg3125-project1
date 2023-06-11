5b292997b6f7ef5f255162f7a966efe19848b1bb
Akarsh Gharge <akarshgharge20@gmail.com>
2023-06-11 11:23:49 -0400

    Return to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename='/seg3125-project1'>
       <App/>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
